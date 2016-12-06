026801119f43062501d27b137d9c34f89ce87a13
Greg Eakin <greg@gdbtech.info>
2016-12-05 22:43:17 -0800

    Remove old JiffMarkerTests.cs
    Move test out of StartOfScanTests

--- PhotoTests/Jpeg/JpegTags/StartOfScanTests.cs
+++ PhotoTests/Jpeg/JpegTags/StartOfScanTests.cs
@@ -13,35 +13,5 @@ namespace PhotoTests.Jpeg.JpegTags
     [TestClass]
     public class StartOfScanTests
     {
-        [TestMethod]
-        public void CtorTest()
-        {
-            var data = new byte[]
-            {
-                0xFF, 0xDA, 0x00, 0x0A, 0x02, 0x01, 0x34, 0x02, 0x65,
-                0xAA, 0xBB, 0xCC
-            };
-
-            using (var memory = new MemoryStream(data))
-            using (var reader = new BinaryReader(memory))
-            {
-                var startOfScan = new StartOfScan(reader);
-
-                Assert.AreEqual(0x000A, startOfScan.Length);
-
-                var components = startOfScan.Components;
-                Assert.AreEqual(2, components.Length);
-                Assert.AreEqual(01, components[0].Id);
-                Assert.AreEqual(03, components[0].Dc);
-                Assert.AreEqual(04, components[0].Ac);
-                Assert.AreEqual(02, components[1].Id);
-                Assert.AreEqual(06, components[1].Dc);
-                Assert.AreEqual(05, components[1].Ac);
-
-                Assert.AreEqual(0xAA, startOfScan.Bb1);
-                Assert.AreEqual(0xBB, startOfScan.Bb2);
-                Assert.AreEqual(0xCC, startOfScan.Bb3);
-            }
-        }
     }
 }
--- PhotoTests/Jpeg/StartOfScanTests.cs
+++ PhotoTests/Jpeg/StartOfScanTests.cs
@@ -17,7 +17,7 @@ namespace PhotoTests.Jpeg
     {
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void BadMark()
+        public void BadMarkTest()
         {
             var data = new byte[] { 0x00, 0x00 };
             using (var memory = new MemoryStream(data))
@@ -29,7 +29,7 @@ namespace PhotoTests.Jpeg
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void BadTag()
+        public void BadTagTest()
         {
             var data = new byte[] { 0xFF, 0x00 };
             using (var memory = new MemoryStream(data))
@@ -40,7 +40,7 @@ namespace PhotoTests.Jpeg
         }
 
         [TestMethod]
-        public void Mark()
+        public void MarkTest()
         {
             var data = new byte[] { 0xFF, 0xDA, 0x00, 0x06, 0x00, 0x00, 0x00, 0x00 };
             using (var memory = new MemoryStream(data))
@@ -52,7 +52,7 @@ namespace PhotoTests.Jpeg
         }
 
         [TestMethod]
-        public void Tag()
+        public void TagTest()
         {
             var data = new byte[] { 0xFF, 0xDA, 0x00, 0x06, 0x00, 0x00, 0x00, 0x00 };
             using (var memory = new MemoryStream(data))
@@ -60,6 +60,37 @@ namespace PhotoTests.Jpeg
             {
                 var startOfScan = new StartOfScan(reader);
                 Assert.AreEqual(0xDA, startOfScan.Tag);
+            }
+        }
+
+        [TestMethod]
+        public void ComponetTest()
+        {
+            var data = new byte[]
+            {
+                0xFF, 0xDA, 0x00, 0x0A, 0x02, 0x01, 0x34, 0x02, 0x65,
+                0xAA, 0xBB, 0xCC
+            };
+
+            using (var memory = new MemoryStream(data))
+            using (var reader = new BinaryReader(memory))
+            {
+                var startOfScan = new StartOfScan(reader);
+
+                Assert.AreEqual(0x000A, startOfScan.Length);
+
+                var components = startOfScan.Components;
+                Assert.AreEqual(2, components.Length);
+                Assert.AreEqual(01, components[0].Id);
+                Assert.AreEqual(03, components[0].Dc);
+                Assert.AreEqual(04, components[0].Ac);
+                Assert.AreEqual(02, components[1].Id);
+                Assert.AreEqual(06, components[1].Dc);
+                Assert.AreEqual(05, components[1].Ac);
+
+                Assert.AreEqual(0xAA, startOfScan.Bb1);
+                Assert.AreEqual(0xBB, startOfScan.Bb2);
+                Assert.AreEqual(0xCC, startOfScan.Bb3);
             }
         }
     }
